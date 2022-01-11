f5ed6430ecbadc062268cc7632aa2845bf706498
Chirayu Sharma <thechirayu02@gmail.com>
2022-01-11 13:13:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/location_tracker/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.location_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.location_tracker.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
